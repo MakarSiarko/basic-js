4223f8db141a5af3a5e781db384a7dc818681e75
Makar <cerko2002@gmail.com>
2025-01-06 18:48:32 +0300

    feat: solve simple-chain, recursive-depth, extended repeater and vigenere cipher task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,40 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  const addition =
+    options.addition !== undefined ? String(options.addition) : "";
+  const separator = options.separator !== undefined ? options.separator : "+";
+  const additionSeparator =
+    options.additionSeparator !== undefined ? options.additionSeparator : "|";
+  const additionRepeatTimes =
+    options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+  const fullAddition = Array(additionRepeatTimes)
+    .fill(addition)
+    .join(additionSeparator);
+  const repeatTimes =
+    options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  const fullStr = Array(repeatTimes)
+    .fill(str + fullAddition)
+    .join(separator);
+  return fullStr;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
